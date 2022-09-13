c7d4ada18434b196c22d86d1afa6dd878e33bdc4
vomkaraxioned <omkarv@axioned.com>
2022-09-13 18:55:23 +0530

    changes merged

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,8 +10,6 @@ setInterval(sliding, 2000);
 
 function sliding() {
     currentScroll = currentScroll + window.innerWidth;
-    console.log("scroll:" + slider.scrollLeft)
-    console.log("currentscroll:" + currentScroll);
     if (currentScroll > maxScroll) {
         currentScroll = 0;
     }
